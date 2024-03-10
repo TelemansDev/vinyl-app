9e8c29cdd65df3f92527ba1ed64e27011e162862
Adam Chabrzyk <ch-adam@o2.pl>
2024-03-10 17:40:42 +0100

    Add routing for browse

--- symfony/src/Controller/VinylController.php
+++ symfony/src/Controller/VinylController.php
@@ -3,14 +3,29 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\UnicodeString;
 
-class VinylController
+class VinylController extends AbstractController
 {
     #[Route(path:"/", name:"homepage")]
     public function homepage(): Response
     {
         return new Response('Testowa wiadomość');
+    }
+
+    #[Route(path:'/browse/{slug}', name:'browse')]
+    public function browse(Request $request, string $slug = ''): Response
+    {
+        if ($slug) {
+            $unicodeSlug = new UnicodeString($slug);
+            $title = 'Genre: ' . $unicodeSlug->title(true);
+        } else {
+            $title = 'All genres';
+        }
+
+        return new Response($title);
     }
 }

0c6e98ca8f72ffd248c46a0cae0a93136b3f3ee2
Adam Chabrzyk <ch-adam@o2.pl>
2024-03-14 17:28:58 +0100

    Add homepage view

--- symfony/src/Controller/VinylController.php
+++ symfony/src/Controller/VinylController.php
@@ -13,7 +13,19 @@ class VinylController extends AbstractController
     #[Route(path:"/", name:"homepage")]
     public function homepage(): Response
     {
-        return new Response('Testowa wiadomość');
+        $tracks = [
+            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['song' => 'Waterfalls', 'artist' => 'TLC'],
+            ['song' => 'Creep', 'artist' => 'Radiohead'],
+            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
+        ];
+
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'PB & Jams',
+            'tracks' => $tracks,
+        ]);
     }
 
     #[Route(path:'/browse/{slug}', name:'browse')]

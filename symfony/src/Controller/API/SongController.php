9ec77d1df25d07e44203168c4b1a591bf974615c
Adam Chabrzyk <ch-adam@o2.pl>
2024-03-22 20:25:08 +0100

    Add logger in SongController

--- symfony/src/Controller/API/SongController.php
+++ symfony/src/Controller/API/SongController.php
@@ -4,14 +4,15 @@ declare(strict_types=1);
 
 namespace App\Controller\API;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SongController extends AbstractController
 {
-    #[Route(path:"/api/songs/{id}", name:"api_get_song")]
-    public function getSong(int $id): JsonResponse
+    #[Route(path:"/api/songs/{id<\d+>}", name:"api_get_song", methods: 'GET')]
+    public function getSong(int $id, LoggerInterface $logger): JsonResponse
     {
          // TODO query the database
          $song = [
@@ -19,6 +20,8 @@ class SongController extends AbstractController
             'name' => 'Waterfalls',
             'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
         ];
+
+        $logger->info('Returning API response for ' . $id);
         
         return new JsonResponse($song);
     }

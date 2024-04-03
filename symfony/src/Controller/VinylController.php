832699b260eba55d4dfeef4fee2a451ea285b54a
Adam Chabrzyk <ch-adam@o2.pl>
2024-04-03 21:58:00 +0200

    Add download mixes from github file

--- symfony/src/Controller/VinylController.php
+++ symfony/src/Controller/VinylController.php
@@ -2,13 +2,19 @@
 
 namespace App\Controller;
 
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\UnicodeString;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class VinylController extends AbstractController
 {
+    public function __construct(
+        private readonly HttpClientInterface $httpClient,
+    ) {}
+
     #[Route(path:"/", name:"homepage")]
     public function homepage(): Response
     {
@@ -49,26 +55,12 @@ class VinylController extends AbstractController
 
     private function getMixes(): array
     {
-        // temporary fake "mixes" data
-        return [
-            [
-                'title' => 'PB & Jams',
-                'trackCount' => 14,
-                'genre' => 'Rock',
-                'createdAt' => new \DateTime('2021-10-02'),
-            ],
-            [
-                'title' => 'Put a Hex on your Ex',
-                'trackCount' => 8,
-                'genre' => 'Heavy Metal',
-                'createdAt' => new \DateTime('2022-04-28'),
-            ],
-            [
-                'title' => 'Spice Grills - Summer Tunes',
-                'trackCount' => 10,
-                'genre' => 'Pop',
-                'createdAt' => new \DateTime('2019-06-20'),
-            ],
-        ];
+        try {
+            $response = $this->httpClient->request('GET', 'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json');
+
+            return $response->toArray();
+        } catch (Exception) {
+            return [];
+        }
     }
 }

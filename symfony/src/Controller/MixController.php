a8c425ef7d68961a0b51978e207b2a01dfeb5f71
Adam Chabrzyk <ch-adam@o2.pl>
2024-07-03 20:38:44 +0200

    Add single mix display

--- symfony/src/Controller/MixController.php
+++ symfony/src/Controller/MixController.php
@@ -12,8 +12,12 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MixController extends AbstractController
 {
-    #[Route('/mix/new', 'addNewMix')]
-    public function new(EntityManagerInterface $entityManager): Response
+    public function __construct(
+        private EntityManagerInterface $entityManager
+    ) {}
+
+    #[Route('/mix/new', 'mix_add_new')]
+    public function new(): Response
     {
         $mix = new VinylMix();
         $mix->setTitle('Do you Remember... Phil Collins?!');
@@ -25,11 +29,19 @@ class MixController extends AbstractController
         $mix->setTrackCount(rand(5, 20));
         $mix->setVotes(rand(-50, 50));
 
-        $entityManager->persist($mix);
-        $entityManager->flush();
+        $this->entityManager->persist($mix);
+        $this->entityManager->flush();
 
         return new Response(
           sprintf('Mix %d is %d tracks of pure 80\'s haven', $mix->getId(), $mix->getTrackCount())
         );
+    }
+
+    #[Route('/mix/{id}', 'mix_show')]
+    public function show(VinylMix $mix): Response
+    {
+        return $this->render('mix/show.html.twig', [
+            'mix' => $mix
+        ]);
     }
 }

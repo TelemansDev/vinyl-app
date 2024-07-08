15e1035dc2ad82ee63f10edbd255de3bf95dbbf6
Adam Chabrzyk <chadam72@gmail.com>
2024-07-08 21:29:16 +0200

    Contributor test

--- symfony/src/Factory/VinylMixFactory.php
+++ symfony/src/Factory/VinylMixFactory.php
@@ -31,6 +31,7 @@ final class VinylMixFactory extends PersistentProxyObjectFactory
      */
     protected function defaults(): array|callable
     {
+        dd();
         return [
             'genre' => self::faker()->randomElement(['pop', 'rock']),
             'title' => self::faker()->words(5, true),
